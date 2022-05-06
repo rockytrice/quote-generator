2908149045009ebf654b0fd6d3c413e58c608f54
Roderick Trice <rocky_trice@yahoo.com>
2022-05-06 00:04:18 -0400

    add loading spinner when fetching quotes

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // fetching data from api using async await
 let apiQuotes = [];
 
+//show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loader
+function loadingComplete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // SHOW NEW QUOTE
 function newQuote() {
+  loading();
   // Pick a random quote from api quote array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if author field is blank and replace with 'Unknown'
@@ -23,10 +37,13 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // set quote and hide the loader
   quoteText.textContent = quote.text;
+  loadingComplete();
 }
 // fetch quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     //  this constant will not  be populated until it has some data fetched from our API
@@ -39,7 +56,6 @@ async function getQuotes() {
     console.log(err);
   }
 }
-
 // Tweet Quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
